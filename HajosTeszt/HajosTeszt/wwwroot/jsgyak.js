fd9389cb5def40a4e3fd5d2d948de98fd21f02e6
Zsombor Tóth <zsombor.toth@stud.uni-corvinus.hu>
2021-03-18 17:34:18 +0100

    szorzotabla

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,36 @@
+﻿let hova = document.getElementById("ide");
+
+for (var s = 0; s < 10; s++) {
+    let sor = document.createElement("div");
+    hova.appendChild(sor)
+    sor.classList.add("sor");
+    for (var o = 0; o < 10; o++) {
+        let szám = document.createElement("div");
+        sor.appendChild(szám);
+        szám.classList.add("doboz");
+        szám.innerText = (s + 1) * (o + 1);
+        szám.style.background = `rgb(${255-(255 / 10 * s)}, 0, 0)`;
+    }
+}
+
+let oda = document.getElementById("idepascal");
+
+for (var s = 0; s < 10; s++) {
+    let sor = document.createElement("div");
+    oda.appendChild(sor);
+    sor.classList("sor");
+    for (var o = 0; o < s + 1; o++) {
+        let szám = document.createElement("div");
+        sor.appendChild(szám);
+        szám.classList.add("doboz");
+        szám.innerText = faktorialisR;
+    }
+}
+var faktorialisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * faktorialisR(n - 1);
+    }
+}
